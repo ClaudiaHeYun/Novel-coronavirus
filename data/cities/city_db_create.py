e577c0c1e10c0222c1bdbd2e279d9de6814b6f7b
Quinn Abrams <qcabrams@gmail.com>
2020-03-08 14:01:10 -0400

    initial commit for db_creation script

--- data/cities/city_db_create.py
+++ data/cities/city_db_create.py
@@ -1,107 +1,66 @@
-from bs4 import BeautifulSoup as BS
-import requests
 import sqlite3
+import csv
 
-### IEX TRADING API METHODS ###
-IEX_TRADING_URL = "https://cloud.iexapis.com/stable/stock/"
-base_url = "https://cloud.iexapis.com/"
+with open("city-populations.csv") as cities_csv:
+    city_reader = csv.DictReader(cities_csv)
 
+# with open("PEP_2018_PEPANNRSIP.US12A.csv") as states_csv:
+#     state_reader = csv.DictReader(cities_csv)
 
-### YAHOO FINANCE SCRAPING
-MOST_ACTIVE_STOCKS_URL = "https://cs.brown.edu/courses/csci1951-a/resources/stocks_scraping_2020.html"
-
-### Register at IEX to receive your unique token
-TOKEN = 'pk_61d134213f2c4bb988af57dbe09b9871'
-
-# TODO: Use BeautifulSoup and requests to collect data required for the assignment.
-active_res = requests.get(MOST_ACTIVE_STOCKS_URL)
-active_soup = BS(active_res.text, 'html.parser')
-table = active_soup.find('table').find_all('tr')
-
-companies = {}
-quotes = {}
-for row in table[1:]:
-    tds = row.find_all('td')
-    (_, name, symbol, price, change, change_pct, volume, hq) = tds
-    # Cleaning and type conversion
-    name = name.find('a').string
-    symbol = symbol.string.lower().strip()
-    price = float(price.string.replace(",", ""))
-    change = float(change.string.replace("+", ""))
-    change_pct = float(change_pct.string[:-1].replace("+", ""))
-    volume = float(volume.string[:-1])
-    hq = hq.string.lower().strip()
-    # company :: [ skip, name :: String, ticker_symbol :: String, price :: Decimal, change_percentage :: Decimal, volume :: Decimal, HQ :: String] 
-    companies[symbol] = [name, hq]
-    quotes[symbol] = [price, None, None, volume, change_pct]
-
-# "genTbl closedTbl elpTbl elp25 crossRatesTbl"
-
-#TODO: Use IEX trading API to collect sector and news data.
-for symbol in companies:
-    # Request the most recent 5 day period because this is what is required
-    iex_res = requests.get(f"{IEX_TRADING_URL}{symbol}/chart/5d",
-                            params={"chartCloseOnly": True, "token": TOKEN})
-    # Request the number of recent articles about a company
-    news_res = requests.get(f"{IEX_TRADING_URL}{symbol}/news/last/10",
-                            params={"token": TOKEN})
-    if iex_res.status_code != 200 or news_res.status_code != 200:
-        print(f"{iex_res.status_code} error from stock endpoint for {symbol}")
-        print(f"{news_res.status_code} error from news endpoint for {symbol}")
-        del companies[symbol]
-        del quotes[symbol]
-    else:
-        iex_json = iex_res.json()
-        prices = []
-        for day in iex_json:
-            prices.append(day['close'])
-        avg_price = sum(prices) / len(prices)
-        quotes[symbol][1] = avg_price
-
-        DATE_CUTOFF = 1580515200000
-        count = 0
-        for article in news_res.json():
-            if article['datetime'] >= DATE_CUTOFF:
-                count += 1
-        quotes[symbol][2] = count
-
+with open("country-populations.csv") as countries_csv:
+    country_reader = csv.DictReader(cities_csv)
 
 # Create connection to database
 conn = sqlite3.connect('data.db')
 c = conn.cursor()
 
 # Delete tables if they exist
-c.execute('DROP TABLE IF EXISTS "companies";')
-c.execute('DROP TABLE IF EXISTS "quotes";')
+c.execute('DROP TABLE IF EXISTS "cities";')
+c.execute('DROP TABLE IF EXISTS "states";')
+c.execute('DROP TABLE IF EXISTS "countries";')
 
 #TODO: Create tables in the database and add data to it. REMEMBER TO COMMIT
-symbol_type = "char(15)"
-c.execute('''CREATE TABLE companies (
-    symbol text NOT NULL, 
-    name text, 
-    location text,
-    PRIMARY KEY (symbol)
+
+# Useful column names: NAME,STNAME,POPESTIMATE2018
+c.execute('''CREATE TABLE cities (
+    name text NOT NULL, 
+    state text NOT NULL, 
+    population INTEGER NOT NULL
+    PRIMARY KEY (name)
+    FOREIGN KEY (state) REFERENCES states(name)
 )''')
 
-c.execute('''CREATE TABLE quotes (
-    symbol text NOT NULL,
-    price float,
-    avg_price float,
-    num_articles int,
-    volume float,
-    change_pct float,
-    PRIMARY KEY (symbol)
+c.execute('''CREATE TABLE states (
+    name text NOT NULL, 
+    population INTEGER NOT NULL, 
+    PRIMARY KEY (name)
+)''')
+
+# area in square kilometers
+# density in persons per square kilometer
+# Column names: Country,Year,Population,"Area (sq. km.)","Density (persons per sq. km.)"
+c.execute('''CREATE TABLE countries (
+    name text NOT NULL,
+    year INTEGER,
+    population INTEGER NOT NULL,
+    area REAL NOT NULL,
+    density REAL NOT NULL,
+    PRIMARY KEY (name)
 )''')
 
 conn.commit()
 
-for symbol, company in companies.items():
-    (name, location) = company
-    c.execute("INSERT INTO companies VALUES (?, ?, ?)", (symbol, name, location))
+for row in city_reader:
+    c.execute("INSERT INTO cities VALUES (?, ?, ?)", (
+        row["NAME"], row["STNAME"], row["POPESTIMATE2018"]))
 
-for symbol, quote in quotes.items():
-    (price, avg_price, num_articles, volume, change_pct) = quote
-    c.execute("INSERT INTO quotes VALUES (?, ?, ?, ?, ?, ?)", (symbol, price, avg_price, num_articles, volume, change_pct))
+for row in country_reader:
+    c.execute("INSERT INTO companies VALUES (?, ?, ?, ?, ?)", (
+        row["Country"], 
+        row["Year"], 
+        row["Population"], 
+        row["Area (sq. km.)"], 
+        row["Density (persons per sq. km.)"))
 
 conn.commit()
 conn.close()
