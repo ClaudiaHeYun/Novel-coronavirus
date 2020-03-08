4359adbd2e45d58b263fc7eaea845ea32c6f1cef
Quinn Abrams <qcabrams@gmail.com>
2020-03-08 14:14:56 -0400

    db_creation script written

--- data/population/city_db_create.py
+++ data/population/city_db_create.py
@@ -1,14 +1,14 @@
 import sqlite3
 import csv
 
-with open("city-populations.csv") as cities_csv:
-    city_reader = csv.DictReader(cities_csv)
+cities_csv = open("./city-populations.csv")
+city_reader = csv.DictReader(cities_csv)
 
 # with open("PEP_2018_PEPANNRSIP.US12A.csv") as states_csv:
 #     state_reader = csv.DictReader(cities_csv)
 
-with open("country-populations.csv") as countries_csv:
-    country_reader = csv.DictReader(cities_csv)
+countries_csv = open("./country-populations.csv")
+country_reader = csv.DictReader(countries_csv)
 
 # Create connection to database
 conn = sqlite3.connect('data.db')
@@ -22,16 +22,16 @@ c.execute('DROP TABLE IF EXISTS "countries";')
 #TODO: Create tables in the database and add data to it. REMEMBER TO COMMIT
 
 # Useful column names: NAME,STNAME,POPESTIMATE2018
-c.execute('''CREATE TABLE cities (
-    name text NOT NULL, 
-    state text NOT NULL, 
-    population INTEGER NOT NULL
-    PRIMARY KEY (name)
+c.execute('''CREATE TABLE cities(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL, 
+    state TEXT NOT NULL, 
+    population INTEGER NOT NULL,
     FOREIGN KEY (state) REFERENCES states(name)
 )''')
 
-c.execute('''CREATE TABLE states (
-    name text NOT NULL, 
+c.execute('''CREATE TABLE states(
+    name TEXT NOT NULL, 
     population INTEGER NOT NULL, 
     PRIMARY KEY (name)
 )''')
@@ -39,8 +39,8 @@ c.execute('''CREATE TABLE states (
 # area in square kilometers
 # density in persons per square kilometer
 # Column names: Country,Year,Population,"Area (sq. km.)","Density (persons per sq. km.)"
-c.execute('''CREATE TABLE countries (
-    name text NOT NULL,
+c.execute('''CREATE TABLE countries(
+    name TEXT NOT NULL,
     year INTEGER,
     population INTEGER NOT NULL,
     area REAL NOT NULL,
@@ -51,11 +51,11 @@ c.execute('''CREATE TABLE countries (
 conn.commit()
 
 for row in city_reader:
-    c.execute("INSERT INTO cities VALUES (?, ?, ?)", (
+    c.execute("INSERT INTO cities (name, state, population) VALUES (?, ?, ?)", (
         row["NAME"], row["STNAME"], row["POPESTIMATE2018"]))
 
 for row in country_reader:
-    c.execute("INSERT INTO companies VALUES (?, ?, ?, ?, ?)", (
+    c.execute("INSERT INTO countries VALUES (?, ?, ?, ?, ?)", (
         row["Country"], 
         row["Year"], 
         row["Population"], 
@@ -64,3 +64,6 @@ for row in country_reader:
 
 conn.commit()
 conn.close()
+
+cities_csv.close()
+countries_csv.close()
