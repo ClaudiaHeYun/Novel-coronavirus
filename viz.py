6885d085344174803d9a28784451cedd49a0db2b
Quinn Abrams <qcabrams@gmail.com>
2020-04-10 16:57:05 -0400

    add a prototype viz file

---
+++ viz.py
@@ -0,0 +1,4 @@
+from analysis import get_connectedness_data
+
+# Get connectedness will give you an array with all the data necessary for viz
+# Feel free to tweak get_connectedness_data if you need more data though
