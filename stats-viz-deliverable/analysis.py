37ee16bc262a964d32f63bafb4b5b4bc7a8e75de
varunsn <varun.senthilnathan@gmail.com>
2020-05-04 23:10:41 -0400

    Created dataframe with merged routes and population data

--- stats-viz-deliverable/analysis.py
+++ stats-viz-deliverable/analysis.py
@@ -473,7 +473,38 @@ def overall_multiregression(x_train, x_test, y_train, y_test):
 	# print(results.summary())
 	return
 
+def create_population_and_routes_dataframe(routes, route_countries, population_data, pop_countries):
+	"""Takes in routes and population data and returns a merged data frame
+	"""
+	route_countries_to_remove  = route_countries -  pop_countries 
+	new_routes = []
+	for row in routes:
+		curr_country = row[0]
+		flag = True
+		for c_to_remove in route_countries_to_remove:
+			if curr_country == c_to_remove:
+				flag = False
+				break
+		if flag:
+			new_routes.append(row)
 
+	pop_countries_to_remove  = pop_countries - route_countries
+	new_population = []
+	for row in population_data:
+		curr_country = row[0]
+		flag = True
+		for c_to_remove in pop_countries_to_remove:
+			if curr_country == c_to_remove:
+				flag = False
+				break
+		if flag:
+			new_population.append(row)
+
+	rdf = pd.DataFrame(new_routes)
+	rdf.columns =['country', 'routes']
+	
+	pdf = pd.DataFrame(new_population)
+	pdf.columns =['country', 'population', 'density']
 
 if __name__ == "__main__":
 	pp = pprint.PrettyPrinter()
